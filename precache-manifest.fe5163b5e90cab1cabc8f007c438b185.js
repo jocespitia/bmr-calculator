1c73e29895a3dc641acd00743a4913a6827b1d3c
jocespitia <jocelyn.espitia@gmail.com>
2019-09-25 18:27:46 -0700

    Updates

---
+++ precache-manifest.fe5163b5e90cab1cabc8f007c438b185.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "065eecb71e2eab43cae6f84cd6a86c66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b58775aed90db3f21d5d",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "f65d247f2a3f80dce3e6",
+    "url": "/static/js/2.133dc355.chunk.js"
+  },
+  {
+    "revision": "b58775aed90db3f21d5d",
+    "url": "/static/js/main.8a56d402.chunk.js"
+  },
+  {
+    "revision": "a6dcd9df65daba281b9f",
+    "url": "/static/js/runtime~main.0dd806cc.js"
+  }
+]);
